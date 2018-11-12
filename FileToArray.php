5594b8756423c34af65f2402ac5aa2449ccde991
Munky Mensch <j.tothdavid@gmail.com>
2018-11-12 18:55:37 +0100

    reusable FileToArray without cli

---
+++ FileToArray.php
@@ -0,0 +1,22 @@
+<?php
+if ($argv && $argv[0] && realpath($argv[0]) === __FILE__) {
+
+    function __autoload($DavidIniParser)
+    {
+        require_once "DavidIniParser.php";
+    }
+
+    class DavidFileToArray
+    {
+        public function FileToArray($filename)
+        {
+            $obj = new DavidIniParser;
+            return $obj->IniToArray($filename);
+        }
+    }
+
+    $test = new DavidFileToArray();
+    $testArray = $test->FileToArray("test4.ini");
+    print_r($testArray);
+
+}
